22bdc0e7fed5c40016b235b054e522e522e69417
Drakeen <info@howardfms.com>
2019-10-14 14:34:39 -0600

    share by email message

--- assets/javascript/share.js
+++ assets/javascript/share.js
@@ -3,7 +3,7 @@
 function sendMail() {
     email = $("#email-input").val();
     console.log(email);
-    window.open('mailto:' +email +'?subject=Join%20us%20at%20RPG%20Buddy&body=https://gamlilorien.github.io/rpg-buddy/');
+    window.open('mailto:' +email +'?subject=Join%20us%20at%20RPG%20Buddy&body=Hello,%20Jesse%20has%20invited%20you%20to:%20https://gamlilorien.github.io/rpg-buddy/');
     //then empty the form value
     $("#email-input").empty();
 }
